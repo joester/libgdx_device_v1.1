459364d2936cf6e3f2e8308a6b45384b4c8cb60f
EyeWumbo <sbzhang@uci.edu>
2013-10-12 18:58:06 -0700

    Fixed some HEADer bugs

--- src/menu/BaseState.java
+++ src/menu/BaseState.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package menu;
 
 import java.util.HashMap;
@@ -30,29 +30,3 @@ public abstract class BaseState {
 	public abstract void dispose();
 	
 }
-=======
-package menu;
-
-import sounds.SoundSystem;
-import game.controls.Controller;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public abstract class BaseState {
-	
-	protected SoundSystem sound;
-	protected StateManager state;
-	Controller controller;
-	
-	public BaseState(StateManager state, SoundSystem sound){
-		this.state = state;
-		this.sound = sound;
-		
-	}
-	
-	public abstract void render(SpriteBatch batch);
-	public abstract void create();
-	public abstract void dispose();
-	
-}
->>>>>>> branch 'master' of https://github.com/EyeWumbo/libgdx_device_v1.1.git
--- src/menu/OptionsScreen.java
+++ src/menu/OptionsScreen.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package menu;
 
 import java.io.IOException;
@@ -53,63 +53,5 @@ public class OptionsScreen extends BaseState{
 		// TODO Auto-generated method stub
 		
 	}
-	
-	
+
 }
-=======
-package menu;
-
-import java.io.IOException;
-
-import sounds.SoundSystem;
-
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class OptionsScreen extends BaseState{
-	
-	Button button1 = null;
-	Button button2 = null;
-	Sprite spr;
-	StateManager state;
-	static OptionsContainer opCon = new OptionsContainer();
-	
-	public OptionsScreen(StateManager state, SoundSystem sound){
-		super(state, sound);
-		create();		
-	}
-
-	@Override
-	public void render(SpriteBatch batch) {
-		// TODO Auto-generated method stub
-		spr = new Sprite(state.graphics.ID(203));
-		batch.draw(spr, 0, 0);
-		if(button1.update()){
-			opCon.optionsBool.put("hai", !opCon.optionsBool.get("hai"));
-			try {
-				opCon.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println(opCon.optionsBool.get("hai"));
-		}
-		if(button2.update()){
-			
-		}
-	}
-
-	@Override
-	public void create() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-}
->>>>>>> branch 'master' of https://github.com/EyeWumbo/libgdx_device_v1.1.git
